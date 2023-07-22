5729659e357c96d275826e8b118a398d053f866c
Rohan <rohancrrm@gmail.com>
2023-07-22 15:51:47 +0530

    Shirt and CameraRig done

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -14,15 +14,42 @@ const Shirt = () => {
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
 
+  useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta)); // Ensuring that the color change happens smoothly
+
+  const stateString = JSON.stringify(state); // to make sure that the shirt updates properly every time
+
   return (
-    <group>
+    <group key={stateString}>
       <mesh
         castShadow
         geometry={nodes.T_Shirt_male.geometry}
         material={materials.lambert1}
         material-roughness={1}
         dispose={null}
-      ></mesh>
+      >
+      {/* the below code is for the texture decal on the shirt */}
+        {snap.isFullTexture && (
+          <Decal 
+            position={[0,0,0]}
+            rotation={[0,0,0]}
+            scale={1}
+            map={fullTexture}
+          />
+        )}
+
+      {/* the below code is for the logo decal on the shirt */}
+      {snap.isLogoTexture && (
+          <Decal 
+            position={[0,0.04,0.15]} // Trial and error nums
+            rotation={[0,0,0]}
+            scale={0.15}
+            map={logoTexture}
+            // map-anisotropy={16} // changing the quality of the shirt
+            depthTest={false} // ensuring that the texture always shows up only on the shirt
+            depthWrite={true} 
+          />
+        )}
+      </mesh>
     </group>
   )
 }
--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -8,12 +8,17 @@ import CameraRig from './CameraRig';
 
 const CanvasModel = () => {
   return (
-    <Canvas>
+    <Canvas
+      shadows
+      camera={{ position: [0,0,0], fov: 25 }} // to make the shirt bigger, we bring the camera closer
+      gl={{ preserveDrawingBuffer: true }}
+      className='w-full max-w-full h-full transition-all ease-in'
+    >
       <ambientLight intensity={0.5} />
       {/* <Environment preset='city' /> */}
 
       <CameraRig>
-        {/* <Backdrop />*/}
+        <Backdrop />
         <Center>
           <Shirt />
         </Center> 
